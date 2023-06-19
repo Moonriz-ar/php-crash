c7a862d6fd74957001bbbadc3150ecdf3d73c46e
Andrea Lin <andre93.lin@gmail.com>
2023-06-19 19:14:50 -0300

    add database connection

--- 2-dynamic-web-app/index.php
+++ 2-dynamic-web-app/index.php
@@ -1,4 +1,18 @@
 <?php
 
+require 'Database.php';
 require 'functions.php';
 require 'router.php';
+
+$config = require 'config.php';
+
+// connect to our MySQL database with PDO (PHP data objects, a framework for accessing databases in PHP)
+
+$db = new Database($config);
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
+
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
