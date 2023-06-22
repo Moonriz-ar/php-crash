d268464be98aba4cd250caf96b858eb99cb16760
Andrea Lin <andre93.lin@gmail.com>
2023-06-22 17:27:36 -0300

    intro to form validation

--- 2-dynamic-web-app/views/note.view.php
+++ 2-dynamic-web-app/views/note.view.php
@@ -6,7 +6,7 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
         <p class="mb-6"><a href="/notes" class="text-blue-500 underline">go back...</a></p>
-        <p><?= $note['body'] ?></p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 
