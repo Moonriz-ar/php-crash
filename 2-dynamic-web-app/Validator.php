4a65b1c6cfa4711d5c99208cb70a639ab3cec666
Andrea Lin <andre93.lin@gmail.com>
2023-06-22 17:51:13 -0300

    extract a simple validator class

---
+++ 2-dynamic-web-app/Validator.php
@@ -0,0 +1,16 @@
+<?php
+
+class Validator
+{
+    // static allows calling this method with Validator::string without initializing a Validator instance
+    public static function string($value, $min = 1, $max = INF)
+    {
+        $value = trim($value);
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+
+    public static function email($value)
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+}
