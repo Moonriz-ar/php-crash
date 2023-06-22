2fe2b204824c4fe84cba288a38094a3fef7332b0
Andrea Lin <andre93.lin@gmail.com>
2023-06-22 09:32:23 -0300

    add authorization

--- 2-dynamic-web-app/controllers/note.php
+++ 2-dynamic-web-app/controllers/note.php
@@ -4,7 +4,17 @@ $config = require('config.php');
 $db = new Database($config);
 
 $heading = "Note";
+$currentUserId = 4;
 
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
+
 
 require "views/note.view.php";
