cd922d9b2129665925af3845cbaaa4de5c8697a4
Andrea Lin <andre93.lin@gmail.com>
2023-06-25 11:45:20 -0300

    handle multiple request methods

--- 2-dynamic-web-app/controllers/notes/show.php
+++ 2-dynamic-web-app/controllers/notes/show.php
@@ -4,14 +4,29 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config);
-
 $currentUserId = 4;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // form was submitted, delete the current note
+    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-authorize($note['user_id'] === $currentUserId);
+    authorize($note['user_id'] === $currentUserId);
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    $db->query("delete from notes where id = :id", [
+        'id' => $_POST['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+
+
+    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
