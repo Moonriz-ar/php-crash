067b402a72d5c0b025016704b077f104e49a4e03
Andrea Lin <andre93.lin@gmail.com>
2023-07-03 16:54:19 -0300

    extract form validation and authenticator class

--- 2-dynamic-web-app/Core/Router.php
+++ 2-dynamic-web-app/Core/Router.php
@@ -2,8 +2,6 @@
 
 namespace Core;
 
-use Core\Middleware\Authenticated;
-use Core\Middleware\Guest;
 use Core\Middleware\Middleware;
 
 class Router
@@ -58,7 +56,7 @@ class Router
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
                 Middleware::resolve($route['middleware']);
-                return require base_path($route['controller']);
+                return require base_path('Http/controllers/' . $route['controller']);
             }
         }
 
